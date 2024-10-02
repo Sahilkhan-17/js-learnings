5632408a5309c28a8bba954fbd04b0b4a10cf38d
Sahilkhan-17 <100953878+Sahilkhan-17@users.noreply.github.com>
2024-10-02 06:09:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"   // used to treate all js code as a newer version (Bydefault its there no need to mention).
+
+// this will only work in browser (we have a different syntax for alert() to used in nodejs environment).
+// alert("hello") 
+
+let studentName;  //variable declaration 
+
+let name = "sahil"   //variable declaration + variable initialization
+let age = 18
+let isLoggedIn = true
+let state;
+
+/* List of Datatypes in javascript  */
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
