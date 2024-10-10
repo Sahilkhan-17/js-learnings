8a51c32a897540c3308af1f082bc7fe8ffae1182
Sahilkhan-17 <100953878+Sahilkhan-17@users.noreply.github.com>
2024-10-10 04:32:23 +0000

    for-in loop not woks on map

--- 04_iterations/02_forIn.js
+++ 04_iterations/02_forIn.js
@@ -37,3 +37,15 @@ for (const key in programming) {
     // console.log(`${key} => ${programming[key]}`);
 }
 
+
+// for-in loop does not work with Map 
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    // console.log(key);
+}
